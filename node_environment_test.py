a438ea7665dc64b809507673271815e50ad84d3d
Osama El Hariri <yamsandbread@gmail.com>
2020-05-21 21:10:07 +0300

    Test the trainer with state value experience replay

--- node_environment_test.py
+++ node_environment_test.py
@@ -33,12 +33,26 @@ criticModel = ModelFactory().getCriticModel(stateSpaceSize)
 
 def printAllStateValues():
     values = []
-    for state in range(numberOfStates):
-        oneHotState = numberToOneHot(state)
-        value = criticModel(tf.reshape([oneHotState], [1, numberOfStates]))[
+    for state in range(stateSpaceSize):
+        oneHotState = numberToOneHot(state, stateSpaceSize)
+        value = criticModel(tf.reshape([oneHotState], [1, stateSpaceSize]))[
             0].numpy()[0]
         values.append(value)
     print(values)
+
+
+def printAllStateValuesGrid():
+    values = []
+    for state in range(stateSpaceSize):
+        oneHotState = numberToOneHot(state, stateSpaceSize)
+        # print(tf.reshape([oneHotState], [1, stateSpaceSize]))
+        value = criticModel(tf.reshape([oneHotState], [1, stateSpaceSize]))[
+            0].numpy()[0]
+        values.append(value)
+        # print(value)
+    for i in range(4):
+        print(values[i * 4: (i + 1) * 4])
+    # print(values)
 
 
 def printAllActionProbabilities():
@@ -71,33 +85,88 @@ def printAllActionProbabilities():
 
 # exit()
 
+wins = 0
+count = 0
+criticBag = ExperienceBag()
 while True:
     experienceBag = ExperienceBag()
     for runIndex in range(10):
+        count += 1
         done = False
         state = cartPoleEnv.reset()
         episode = []
         reward = 0
         steps = []
-        while not done or len(episode) > 200:
+        states = []
+        while not done and len(episode) < 20:
             oneHotState = numberToOneHot(state, stateSpaceSize)
 
             prediction = actorModel(tf.reshape([oneHotState], [1, stateSpaceSize]))[
                 0].numpy()
             action = np.random.choice(len(prediction), p=prediction)
+            # action = np.random.choice(
+            #     len(prediction), p=[0.25, 0.25, 0.25, 0.25])
+            # if state == 0:
+            #     action = 2
+            # elif state == 1:
+            #     action = 2
+            # elif state == 2:
+            #     action = 1
+            # elif state == 6:
+            #     action = 1
+            # elif state == 10:
+            #     action = 1
+            # elif state == 14:
+            #     action = 2
             oneHotAction = numberToOneHot(action, len(prediction))
 
             nextState, reward, done, _ = cartPoleEnv.step(action)
             oneHotNextState = numberToOneHot(nextState, stateSpaceSize)
-            episode.append((oneHotState, oneHotAction,
-                            reward, oneHotNextState, done))
-            steps.append("{}, {}".format(state, np.argmax(prediction)))
+
+            # if state == 0 and nextState == 1:
+            #     reward = 0.1
+            # elif state == 1 and nextState == 2:
+            #     reward = 0.1
+            # elif state == 2 and nextState == 6:
+            #     reward = 0.1
+            # elif state == 6 and nextState == 10:
+            #     reward = 0.1
+            # elif state == 10 and nextState == 14:
+            #     reward = 0.1
+            # elif state == 14 and nextState == 15:
+            #     reward = 1
+
+            reward -= 0.01
+            # if state == nextState:
+            #     reward = -1
+
+            # if state in states:
+            #     reward -= 0.01
+            # else:
+            #     states.append(state)
+            if nextState == 15:
+                wins += 1
+
             if done:
                 steps.append(reward)
+
+            episode.append((oneHotState, oneHotAction,
+                            reward, oneHotNextState, done))
+            steps.append("{}, {}, {}".format(
+                state, np.argmax(prediction), reward))
             state = nextState
         experienceBag.addEpisode(episode)
+        criticBag.addEpisode(episode)
         print(steps)
         print("------------------------------------")
-    print("Ep")
-    trainer = ModelTrainer(actorModel, criticModel, experienceBag)
-    trainer.trainActorCritic(100, 16)
+    # print("Ep")
+    printAllStateValuesGrid()
+    print("Ep {}, wins {}".format(count, wins))
+    if len(criticBag.experiences) > 100:
+        allFirstSteps = criticBag.experiences[0].getAllSteps()
+        if allFirstSteps[len(allFirstSteps) - 1].reward > 2:
+            first = criticBag.experiences.pop(0)
+            criticBag.experiences.append(first)
+        criticBag.experiences = criticBag.experiences[1:]
+    trainer = ModelTrainer(actorModel, criticModel, experienceBag, criticBag)
+    trainer.trainActorCritic(10, 16)
--- trajectory_utils/ModelTrainer.py
+++ trajectory_utils/ModelTrainer.py
@@ -3,12 +3,13 @@ import random
 
 
 class ModelTrainer:
-    def __init__(self, actorModel, criticModel, experienceBag):
+    def __init__(self, actorModel, criticModel, experienceBag, criticBag):
         self.actorModel = actorModel
         self.criticModel = criticModel
         self.experienceBag = experienceBag
-        self.criticLearningRate = 0.001
-        self.actorLearningRate = 0.001
+        self.criticBag = criticBag
+        self.criticLearningRate = 0.0001
+        self.actorLearningRate = 0.0001
         self.gamma = 0.99
 
     def trainActorCritic(self, minibatchSize, minibatchTrainCount):
@@ -16,23 +17,26 @@ class ModelTrainer:
         allStates = self.arrayToTFMatrix(
             self.experienceBag.getStates(allSteps))
         currentActionProbabilities = self.actorModel(allStates)
-        currentStateValues = self.criticModel(allStates)
+        # currentStateValues = self.criticModel(allStates)
+
+        allSteps2 = self.criticBag.getAllSteps()
 
         for i in range(len(currentActionProbabilities)):
             allSteps[i].previousActionProbabilities = currentActionProbabilities[i]
-            allSteps[i].previousStateValues = currentStateValues[i]
+            # allSteps[i].previousStateValues = currentStateValues[i]
 
+        self.trainCritic(self.getMinibatch(allSteps2, len(allSteps2) // 3))
         for i in range(minibatchTrainCount):
             minibatch = self.getMinibatch(allSteps, minibatchSize)
-            self.trainOnMinibatch(minibatch)
+            self.trainActor(minibatch)
 
     def getMinibatch(self, allSteps, size):
         random.shuffle(allSteps)
         return allSteps[0:size]
 
-    def trainOnMinibatch(self, minibatch):
-        self.trainActor(minibatch)
-        self.trainCritic(minibatch)
+    # def trainOnMinibatch(self, minibatch):
+    #     self.trainActor(minibatch)
+    #     self.trainCritic(minibatch)
 
     # Train the actor using L_CLIP that is defined in the PPO paper
     def trainActor(self, minibatch):
@@ -72,19 +76,33 @@ class ModelTrainer:
 
     # Train the critic using the squared error
     def trainCritic(self, minibatch):
-        optimizer = tf.keras.optimizers.Adam(lr=self.criticLearningRate)
+        optimizer = tf.keras.optimizers.SGD(lr=self.criticLearningRate)
 
+        # print("-----------Train Critic----------------")
         states = self.arrayToTFMatrix(self.experienceBag.getStates(minibatch))
+        # print(states)
         stateValues = self.arrayToTFMatrix(
             self.experienceBag.getStateValues(minibatch, self.criticModel, self.gamma))
+        # print(stateValues)
+        # print("-----------States and True Values----------------")
 
         with tf.GradientTape() as tape:
             predictions = self.criticModel(states)
+            # print(predictions)
+            # print("-----------Predictions----------------")
 
             predictions = tf.cast(predictions, tf.float64)
+            # stateValues = stateValues - \
+            #     tf.reduce_mean(stateValues) / \
+            #     (tf.math.reduce_std(stateValues) + 1e-10)
             stateValues = tf.cast(stateValues, tf.float64)
 
             loss = tf.keras.losses.mse(stateValues, predictions)
+
+            # loss = tf.math.pow(predictions - stateValues, 2)
+            # loss = stateValues - predictions
+            # print(loss)
+            # print("-----------Losses----------------")
 
         grads = tape.gradient(loss, self.criticModel.trainable_variables)
         optimizer.apply_gradients(
